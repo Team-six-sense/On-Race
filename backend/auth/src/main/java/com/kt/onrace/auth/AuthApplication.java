d387aa8422625132f5580725fbe840dd83d734ca
NSRBSG <nsrbsg@gmail.com>
2026-02-26 10:36:05 +0900

    fix: ComponentScan 에러 해결 (common 모듈 스캔 추가)

--- backend/auth/src/main/java/com/kt/onrace/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/kt/onrace/auth/AuthApplication.java
@@ -3,11 +3,11 @@ package com.kt.onrace.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.kt.onrace.auth", "com.kt.onrace.common"})
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
-	
+
 }
