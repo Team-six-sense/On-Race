238c1612b667e18af0a239465b4390dc902f3ece
gegujin <gegujin7371@naver.com>
2026-03-30 11:49:59 +0900

    Fix : an error caused by an undeclared variable

--- infra/envs/prod/app/app-variables.tf
+++ infra/envs/prod/app/app-variables.tf
@@ -17,3 +17,9 @@ variable "environment" {
   type        = string
   default     = "prod"
 }
+
+variable "namespace" {
+  description = "Kubernetes namespace"
+  type        = string
+  default     = "t6-on-race-prod" # 실제 사용 중인 네임스페이스 명칭으로 수정
+}
