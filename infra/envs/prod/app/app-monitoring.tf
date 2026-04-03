bd39454e02ba00db3c5aeafd70f8224918f2f75a
gegujin <gegujin7371@naver.com>
2026-04-03 18:54:15 +0900

    Fix :  resolve timeouts again in Prometheus2

--- infra/envs/prod/app/app-monitoring.tf
+++ infra/envs/prod/app/app-monitoring.tf
@@ -14,7 +14,7 @@ resource "helm_release" "loki" {
   namespace        = "loki"
   create_namespace = true
   
-  timeout          = 900
+  timeout          = 1500
 
   # [핵심] templatefile을 통해 변수들을 실제 값으로 치환하여 주입
   values = [
