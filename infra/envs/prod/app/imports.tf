78b4f4df1efbabb8b7f97165369eeb2953875cda
gegujin <gegujin7371@naver.com>
2026-04-06 01:32:05 +0900

    fix: finalize infra recovery with verified imports and clean workflow

--- infra/envs/prod/app/imports.tf
+++ infra/envs/prod/app/imports.tf
@@ -14,10 +14,15 @@ import {
   id = "arn:aws:iam::274130523831:policy/t6-on-race-prod-cluster-LokiS3Access"
 }
 
-# 2. CloudFront 공개 키
+# 2. CloudFront 리소스
 import {
   to = aws_cloudfront_public_key.vqa_key
   id = "K2POEQMPBW4Z72"
+}
+
+import {
+  to = aws_cloudfront_key_group.vqa_key_group
+  id = "이곳에_키그룹_ID를_입력하세요" 
 }
 
 # 3. 보안 그룹 (Security Group)
